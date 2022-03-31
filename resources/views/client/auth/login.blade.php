d6a042d8275b13542e4b222912b367b5564a1b71
İbrahim EFE <ibrahim.efee@hotmail.com>
2022-04-01 00:04:17 +0300

    ...

--- resources/views/client/auth/login.blade.php
+++ resources/views/client/auth/login.blade.php
@@ -15,6 +15,7 @@
     <link
         href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&family=Roboto:wght@100;300;400;500;700;900&display=swap"
         rel="stylesheet">
+        <html method="POST">
 </head>
 
 <body  style="height: 100vh;">
